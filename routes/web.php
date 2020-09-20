3fce5cb4dc560dae45ee7059453b51590d7a9306
Jack Tapanut <tapanut.akk@gmail.com>
2020-09-20 23:51:46 +0700

    Add vue ui and auth

--- routes/web.php
+++ routes/web.php
@@ -46,3 +46,10 @@ Route::get('/articles/create', 'ArticlesController@create');
 Route::get('/articles/{article}', 'ArticlesController@show')->name('articles.show');
 Route::get('/articles/{article}/edit', 'ArticlesController@edit');
 Route::put('/articles/{article}', 'ArticlesController@update');
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
