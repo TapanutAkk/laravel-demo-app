8108141cdbc9e66517cdffe704b1a3d521757abd
Jack Tapanut <tapanut.akk@gmail.com>
2020-11-29 00:22:57 +0700

    current version

--- routes/web.php
+++ routes/web.php
@@ -50,6 +50,56 @@ Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
 
-Auth::routes();
-
 Route::get('/home', 'HomeController@index')->name('home');
+
+Route::get('/createlcom/check_prime', function () {
+
+    $result['inputs'] = [3,1,9,10,7,2,5,4,6,8];
+    $result['primes'] = [];
+    $result['number'] = [];
+
+    $check_prime = false;
+    foreach($result['inputs'] as $input){
+        $check_prime = true;
+        if($input == 1){
+            array_push($result['number'],$input);
+        } else {
+            for($i=2;$i<$input;$i++){
+                if($input % $i == 0){
+                    $check_prime = false;
+                    break;
+                }
+            }
+            if($check_prime){
+                array_push($result['primes'],$input);
+            } else {
+                array_push($result['number'],$input);
+            }
+        }
+        
+    }
+    
+    dd($result);
+    
+});
+
+Route::get('/createlcom/start_vowel', function () {
+
+    $result['inputs'] = ['Apple','Banana','Coconut','Elderberry','Fig','Grapes','Mango','Melon','Orange'];
+    $result['search'] = ['A','E','I','O','U'];
+    $result['vowel'] = [];
+    $result['no_vowel'] = [];
+
+    $check_prime = false;
+    foreach($result['inputs'] as $input){
+        if (in_array($input[0], $result['search'])){
+            array_push($result['vowel'],$input);
+        } else {
+            array_push($result['no_vowel'],$input);
+        }
+        
+    }
+    
+    dd($result);
+    
+});
